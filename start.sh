e08e11230fda0aa8c2ea554efd97c6dfac992aba
10102232 <31153532+10102232@users.noreply.github.com>
2020-04-07 23:20:17 +0800

    Update start.sh
    111

--- start.sh
+++ start.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-cd v2ray
-./v2ray&
-sleep 9d
-kill -9 $(ps -ef|grep v2ray|grep -v grep|awk '{print $2}')
-./edge -d n2n0 -a 10.255.255.10 -c fanqiang -k fanqiang -r -b -l 132.232.37.251:10000 -f
-./gost -L=:18080
+000
