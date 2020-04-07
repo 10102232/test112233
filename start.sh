e8972b89c8c56a41dbb45c1d8ad4b9e587d7e063
10102232 <31153532+10102232@users.noreply.github.com>
2020-04-07 22:22:27 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd v2ray
+./v2ray&
+sleep 9d
+kill -9 $(ps -ef|grep v2ray|grep -v grep|awk '{print $2}')
+./edge -a 10.255.255.1 -c fanqiang -k fanqiang -r -b -l 132.232.37.251:10000 -f
